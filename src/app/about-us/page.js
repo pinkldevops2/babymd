b74e7f1af6a72fb02ec99360fa812dac7dcfc88d
Ajith Kumar A R <>
2025-05-24 01:38:55 +0530

    About Us

--- src/app/about-us/page.js
+++ src/app/about-us/page.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1992c1e8952790b7bc92e6ac5fc4c08587e7ef8a1f5dca13cc6ca405e29c9241
-size 226
+oid sha256:21181c7811ca0c15f5eaed84ac7c603298106bde0e99594af560fbed63e88158
+size 240
